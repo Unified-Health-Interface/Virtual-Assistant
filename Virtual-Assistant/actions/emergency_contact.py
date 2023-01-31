2cc7a12d71813176e74b3364378d609aa6355ed2
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 23:10:35 +0530

    Create get all user emergency contacts action

---
+++ Virtual-Assistant/actions/emergency_contact.py
@@ -0,0 +1,24 @@
+from typing import Any, Text, Dict, List
+
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetAllUserEmergencyContacts(Action):
+    def name(self) -> Text:
+        return "action_get_all_user_emergency_contacts"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        contacts = requests.get(f"http://0.0.0.0:8000/emergency-contact/{tracker.sender_id}").json()
+
+        response = "Your emergency contacts are:\n"
+
+        for contact in contacts:
+            response += f"{contact['id']} - {contact['name']} has email {contact['email']} and phone number {contact['phone']}.\n"
+
+        dispatcher.utter_message(text=response)
+
+        return []
