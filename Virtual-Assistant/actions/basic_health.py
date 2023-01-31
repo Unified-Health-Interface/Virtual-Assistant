eb46c2287b70ef23cec1063f421b8b1ad3e41bba
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 22:21:43 +0530

    Create get user basic health details action

---
+++ Virtual-Assistant/actions/basic_health.py
@@ -0,0 +1,21 @@
+from typing import Any, Text, Dict, List
+
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetUserBasicHealthDetails(Action):
+    def name(self) -> Text:
+        return "action_get_user_basic_health_details"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        details = requests.get(f"http://0.0.0.0:8000/basic-health/{tracker.sender_id}").json()
+
+        response = f"Your age is {details['age']}.\nYour date of birth is {details['dob']}.\nYour sex is {details['sex']}.\nYour blood group is {details['blood_group']}"
+
+        dispatcher.utter_message(text=response)
+
+        return []
