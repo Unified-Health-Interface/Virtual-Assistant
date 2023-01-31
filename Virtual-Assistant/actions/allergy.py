7d7a4e7a18a95d1202a07718aa392846831ad7b8
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 17:28:09 +0530

    Create check if user has given allergy action

--- Virtual-Assistant/actions/allergy.py
+++ Virtual-Assistant/actions/allergy.py
@@ -12,14 +12,36 @@ class ActionGetAllUserAllergies(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # dispatcher.utter_message(text="Hello, World!")
-
         allergies = requests.get("http://0.0.0.0:8000/allergy/hari").json()
 
         response = "You are allergic to:\n"
 
         for allergy in allergies:
             response += f"{allergy['id']} - {allergy['allergy']}\n"
+
+        dispatcher.utter_message(text=response)
+
+        return []
+
+
+class ActionCheckIfUserHasGivenAllergy(Action):
+    def name(self) -> Text:
+        return "action_check_if_user_has_given_allergy"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        allergies = requests.get("http://0.0.0.0:8000/allergy/hari").json()
+
+        allergy = next(tracker.get_latest_entity_values("allergy"), None)
+
+        print([i for i in tracker.get_latest_entity_values("allergy")])
+
+        if allergy in [i['allergy'] for i in allergies]:
+            response = f"Yes, you are allergic to {allergy}. Take care!"
+
+        else:
+            response = f"No, you are not allergic to {allergy}. Stay safe!"
 
         dispatcher.utter_message(text=response)
 
