409b3944ecfb3e59a05d4d580cf632d5ffbcba69
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 13:01:22 +0530

    Reformat

--- Virtual-Assistant/actions/user/get_user_details.py
+++ Virtual-Assistant/actions/user/get_user_details.py
@@ -1,12 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 import requests
