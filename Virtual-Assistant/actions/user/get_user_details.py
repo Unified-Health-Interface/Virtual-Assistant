fae0a95660f14025a6a1881b9febc80feb5c52c9
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 12:38:04 +0530

    Create get user details action

---
+++ Virtual-Assistant/actions/user/get_user_details.py
@@ -0,0 +1,41 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetUserDetails(Action):
+    def name(self) -> Text:
+        return "action_get_user_details"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # dispatcher.utter_message(text="Hello, World!")
+
+        details = requests.get("http://0.0.0.0:8000/user/hari").json()
+
+        username = details['username']
+        full_name = details['full_name']
+        user_id = details['id']
+
+        response = \
+            f"""
+Your username is {username}.
+Your full name is {full_name}.
+Your user id is {user_id}.
+"""
+
+        dispatcher.utter_message(text=response)
+
+        return []
