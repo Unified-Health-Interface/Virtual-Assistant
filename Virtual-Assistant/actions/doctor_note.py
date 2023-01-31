af55497db22e45d1ee1138b0049198217d668412
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 23:03:44 +0530

    Create get all user doctor notes action

---
+++ Virtual-Assistant/actions/doctor_note.py
@@ -0,0 +1,25 @@
+
+from typing import Any, Text, Dict, List
+
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetAllUserDoctorNotes(Action):
+    def name(self) -> Text:
+        return "action_get_all_user_doctor_notes"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        notes = requests.get(f"http://0.0.0.0:8000/doctor-note/{tracker.sender_id}").json()
+
+        response = "Your doctors notes are:\n"
+
+        for note in notes:
+            response += f"{note['id']} - {note['doctor']} says {note['note']}.\n"
+
+        dispatcher.utter_message(text=response)
+
+        return []
