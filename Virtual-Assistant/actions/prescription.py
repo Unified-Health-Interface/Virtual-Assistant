960de615f23e12d54a58c0a24ea36be72c096b9d
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 23:19:49 +0530

    Create get all user prescriptions action

---
+++ Virtual-Assistant/actions/prescription.py
@@ -0,0 +1,24 @@
+from typing import Any, Text, Dict, List
+
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetAllUserPrescriptions(Action):
+    def name(self) -> Text:
+        return "action_get_all_user_prescriptions"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        prescriptions = requests.get(f"http://0.0.0.0:8000/prescription/{tracker.sender_id}").json()
+
+        response = "Your emergency contacts are:\n"
+
+        for prescription in prescriptions:
+            response += f"{prescription['id']} - {prescription['doctor']} issued a prescription on {prescription['date_time'].split('T')[0]} to take {prescription['medicines']} everyday at {prescription['date_time'].split('T')[1]}\n"
+
+        dispatcher.utter_message(text=response)
+
+        return []
