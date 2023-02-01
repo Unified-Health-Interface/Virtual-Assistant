696693a621c90a2a58b2987c2f85da684e87e6e0
Harikeshav R <r.harikeshav@gmail.com>
2023-02-01 15:25:46 +0530

    Add phone notifications and email to call all emergency contacts action

--- Virtual-Assistant/actions/emergency_contact.py
+++ Virtual-Assistant/actions/emergency_contact.py
@@ -1,10 +1,48 @@
+import os
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from typing import Any, Text, Dict, List
 
 import requests
+from dotenv import load_dotenv
+from notify_run import Notify
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-from notify_run import Notify
+load_dotenv()
+
+
+def send_notify_notification(endpoint, message):
+    notify = Notify()
+    notify.endpoint = endpoint
+
+    notify.send(message)
+
+
+def send_emails(emails, body):
+    context = ssl.create_default_context()
+
+    subject = "IMPORTANT!!!"
+    body = body
+    sender_email = os.getenv('GMAIL_EMAIL')
+    sender_password = os.getenv('GMAIL_PASSWORD')
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls(context=context)
+        server.login(sender_email, sender_password)
+
+        for receiver_email in emails:
+            message = MIMEMultipart()
+            message["From"] = sender_email
+            message["To"] = receiver_email
+            message["Subject"] = subject
+            message["Bcc"] = receiver_email
+
+            message.attach(MIMEText(body, "plain"))
+
+            server.sendmail(sender_email, receiver_email, message.as_string())
 
 
 class ActionGetAllUserEmergencyContacts(Action):
@@ -33,19 +71,20 @@ class ActionCallAllUserEmergencyContacts(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        response = "Calling all your emergency contacts!"
+
+        dispatcher.utter_message(text=response)
+
         contacts = requests.get(f"http://0.0.0.0:8000/emergency-contact/{tracker.sender_id}").json()
+        full_name = requests.get(f"http://0.0.0.0:8000/user/{tracker.sender_id}").json()['full_name']
         endpoint = requests.get(f"http://0.0.0.0:8000/notify-runner/{tracker.sender_id}").json()['endpoint']
 
         email_addresses = [contact['email'] for contact in contacts]
         phone_numbers = [contact['phone'] for contact in contacts]
 
-        notify = Notify()
-        notify.endpoint = endpoint
+        message = f"{full_name} is in danger! {full_name} needs help!"
 
-        notify.send(f"{tracker.sender_id} is in danger! {tracker.sender_id} needs help!")
-
-        response = "Calling all your emergency contacts!"
-
-        dispatcher.utter_message(text=response)
+        send_notify_notification(endpoint, message)
+        send_emails(email_addresses, message)
 
         return []
