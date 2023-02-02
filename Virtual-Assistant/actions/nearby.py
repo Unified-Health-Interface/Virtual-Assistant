b652f72715274939e487ce359464d1e38976d565
Harikeshav R <r.harikeshav@gmail.com>
2023-02-02 18:25:05 +0530

    Create get all nearby amenities action

---
+++ Virtual-Assistant/actions/nearby.py
@@ -0,0 +1,35 @@
+from typing import Any, Text, Dict, List
+
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionGetAllNearbyAmenities(Action):
+    def name(self) -> Text:
+        return "action_get_all_nearby_amenities"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # allergies = requests.get(f"http://0.0.0.0:8000/allergy/{tracker.sender_id}").json()
+
+        # response = "You are allergic to "
+        #
+        # for allergy in allergies:
+        #     response += f"{allergy['allergy']}, "
+
+        response = ""
+
+        amenity = next(tracker.get_latest_entity_values("amenity"), None)
+
+        request = requests.get("https://nominatim.openstreetmap.org/search",
+                               params={"q": f"{amenity} near the international school bangalore",
+                                       "format": "json"}).json()[0:3]
+
+        for i in request:
+            response += f"{i['display_name']}\n"
+
+        dispatcher.utter_message(text=response)
+
+        return []
